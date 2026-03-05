60df766856c9bed05cf6908afb9987d59e9e2a16
Stas Gusev <bot-io@yandex.ru>
2026-03-05 08:02:05 +0300

    9.5 [dev] Add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
@@ -5,5 +6,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
