4ea154b65ac1fc03f327bb03f6fdca08b9236077
Stas Gusev <bot-io@yandex.ru>
2026-03-05 08:06:06 +0300

    9.6 [dev] Add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -7,5 +8,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
